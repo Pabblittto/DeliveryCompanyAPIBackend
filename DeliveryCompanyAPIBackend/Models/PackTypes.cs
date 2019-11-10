2b5f6789ace0334285f49fe0c073f2490b24ac5b
Pabblo <pawel.mat17@op.pl>
2019-11-10 20:15:04 +0100

    all models added- they are empty !

---
+++ DeliveryCompanyAPIBackend/Models/PackTypes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliveryCompanyAPIBackend.Models
+{
+    public class PackTypes
+    {
+    }
+}
